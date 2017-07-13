24111a39ac94c445a7983649c53301b0ca4cf244
Piotr Betkier <pbetkier@gmail.com>
2017-07-13 16:59:31 +0200

    Devoxx PL '17 talks

---
+++ devoxx-extract.js
@@ -0,0 +1,9 @@
+var jq = document.createElement('script');
+jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";
+document.getElementsByTagName('head')[0].appendChild(jq);
+
+jQuery.noConflict();
+
+var s = "";
+jQuery('.title a').each(function(){s = s + this.text + "\n"});
+s;
