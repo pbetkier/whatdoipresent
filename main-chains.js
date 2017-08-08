e79320a5efd5c5bd1edc01ea3de27df52d871cc2
Piotr Betkier <pbetkier@gmail.com>
2017-08-08 14:18:51 +0200

    working prototype

--- main-chains.js
+++ main-chains.js
@@ -1,10 +1,14 @@
 var fs = require('fs')
 var Chain = require('markov-chains').default;
 
-var array = fs.readFileSync('devoxx17').toString().split("\n");
+var devoxx17 = fs.readFileSync('devoxx17').toString().split("\n");
+var devoxx16 = fs.readFileSync('devoxx16').toString().split("\n");
+var devoxx15 = fs.readFileSync('devoxx15').toString().split("\n");
+var array = devoxx17.concat(devoxx16).concat(devoxx15);
 var corpus = array.map((str) => str.split(' '));
 //console.log(corpus);
 
 var c = new Chain(corpus);
 var s = c.walk().join(" ");
 console.log(s);
+fs.writeFile("json", JSON.stringify(c.toJSON()));
