6ab82c16a7e21340293eaeafe47a2bb1f01b7cf6
Piotr Betkier <pbetkier@gmail.com>
2017-09-03 20:40:59 +0200

    moving stuff around

--- build-chain.js
+++ build-chain.js
@@ -6,15 +6,6 @@ var devoxx16 = fs.readFileSync('data/devoxx16').toString().split("\n");
 var devoxx15 = fs.readFileSync('data/devoxx15').toString().split("\n");
 var array = devoxx17.concat(devoxx16).concat(devoxx15);
 var corpus = array.map((str) => str.split(' '));
-//console.log(corpus);
 
 var c = new Chain(corpus);
-var s = c.walk().join(" ");
-console.log(s);
-//fs.writeFile("json", JSON.stringify(c));
-//var f = fs.readFileSync("json")
-var f = JSON.stringify(c);
-console.log(f);
-console.log(typeof f);
-var c2 = Chain.fromJSON(f);
-console.log(c2.walk().join(" "));
+fs.writeFile("devoxx-markov.json", JSON.stringify(c));
--- whatdoipresent.js
+++
@@ -1,9 +0,0 @@
-//var Chain = require('markov-chains').default;
-
-var array = ['there is no spoon', 'there no is b00n']
-var corpus = array.map((str) => str.split(' '));
-
-var c = new Chain(corpus);
-var s = c.walk().join(" ");
-console.log(s);
-fs.writeFile("json", JSON.stringify(c.toJSON()));
