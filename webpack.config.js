ac8699c43bea9a92e975e5e7d892faa8c9212abb
Piotr Betkier <pbetkier@gmail.com>
2017-09-09 23:22:33 +0200

    dist

--- webpack.config.js
+++ webpack.config.js
@@ -51,18 +51,14 @@ module.exports = {
 if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
+  const MinifyPlugin = require("babel-minify-webpack-plugin");
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"'
       }
     }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),
+    new MinifyPlugin(),
     new webpack.LoaderOptionsPlugin({
       minimize: true
     })
