13c85c0f5c34cb7e44799b9550f5ad37d81dede2
Piotr Betkier <pbetkier@gmail.com>
2017-09-10 10:22:40 +0200

    devoxx extract comment

--- tool/devoxx-extract.js
+++ tool/devoxx-extract.js
@@ -1,3 +1,5 @@
+// execute this in dev tools console on a devoxx schedule page
+
 var jq = document.createElement('script');
 jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";
 document.getElementsByTagName('head')[0].appendChild(jq);
