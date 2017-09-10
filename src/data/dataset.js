1c606612224dda77282e5fe4273f81dd522197f1
Piotr Betkier <pbetkier@gmail.com>
2017-09-10 10:57:59 +0200

    adds devoxx belgium 2016 talks

--- src/data/dataset.js
+++ src/data/dataset.js
@@ -1,8 +1,9 @@
-import devoxx15 from './devoxx15.txt'
-import devoxx16 from './devoxx16.txt'
-import devoxx17 from './devoxx17.txt'
+import devoxx15pl from './devoxx-pl-2015.txt'
+import devoxx16pl from './devoxx-pl-2016.txt'
+import devoxx16be from './devoxx-be-2016.txt'
+import devoxx17pl from './devoxx-pl-2017.txt'
 
-const lists = [devoxx15, devoxx16, devoxx17];
+const lists = [devoxx15pl, devoxx16pl, devoxx16be, devoxx17pl];
 const allTalks = [].concat(...lists.map(x => x.split('\n')));
 
 export default new Set(allTalks);
