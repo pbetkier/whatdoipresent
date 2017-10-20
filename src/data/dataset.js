2e904a58c08111ab8629fa0085350cdc09f8f58c
Chandrasekar Gokulanathan <g.chandrasekar1993@gmail.com>
2017-10-20 23:48:52 +0530

    One time utility to convert the data source from txt to JSON

--- src/data/dataset.js
+++ src/data/dataset.js
@@ -10,3 +10,26 @@ const lists = [devoxx15pl, devoxx16pl, devoxx16be, devoxx17pl, devoxx17be, qconl
 const allTalks = [].concat(...lists.map(x => x.split('\n')));
 
 export default new Set(allTalks);
+
+/*  One time utility to convert the data source from txt to JSON 
+*/
+// function convertTxtToJSON() {
+//     var finalJSON = [];
+//     function converter(lists, confCode, confName) {
+//         var titles = [].concat(...lists.map(x => x.split('\n')));
+//         console.log(titles.length);
+//         titles.forEach(title => {
+//             var tempObj = { "title" : title, "description" : "Sample description", "confCode" : confCode, "confName" : confName };
+//             finalJSON.push(tempObj);
+//         });
+//     };
+//     converter([devoxx15pl], "devoxx-pl-2015", "Devoxx Poland, 2015");
+//     converter([devoxx16pl], "devoxx-pl-2016", "Devoxx Poland, 2016");
+//     converter([devoxx16be], "devoxx-be-2016", "Devoxx Belgium, 2016");
+//     converter([devoxx17pl], "devoxx-pl-2017", "Devoxx Poland, 2017");
+//     converter([devoxx17be], "devoxx-be-2017", "Devoxx Belgium, 2017");
+//     converter([qconlondon16], "qcon-london-2016", "QCon London, 2016");
+//     converter([qconlondon17], "qcon-london-2017", "QCon London, 2017");
+//     console.log(JSON.stringify(finalJSON));
+// };
+// convertTxtToJSON();
