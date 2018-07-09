b13ef1373476ae9f684ba99dd2d9feac55da67d4
Piotr Betkier <pbetkier@gmail.com>
2018-07-09 22:23:49 +0200

    talks from Devoxx 2018 PL & UK

--- src/data/dataset.js
+++ src/data/dataset.js
@@ -1,12 +1,7 @@
-import devoxx15pl from './devoxx-pl-2015.txt'
-import devoxx16pl from './devoxx-pl-2016.txt'
-import devoxx16be from './devoxx-be-2016.txt'
-import devoxx17pl from './devoxx-pl-2017.txt'
-import devoxx17be from './devoxx-be-2017.txt'
-import qconlondon16 from './qcon-london-2016.txt';
-import qconlondon17 from './qcon-london-2017.txt';
+import devoxx18pl from './devoxx-pl-2018.txt';
+import devoxx18uk from './devoxx-uk-2018.txt';
 
-const lists = [devoxx15pl, devoxx16pl, devoxx16be, devoxx17pl, devoxx17be, qconlondon16, qconlondon17];
+const lists = [devoxx18pl, devoxx18uk];
 const allTalks = [].concat(...lists.map(x => x.split('\n')));
 
 export default new Set(allTalks);
---
+++ tool/devoxx-2018-extract.js
@@ -0,0 +1,11 @@
+// execute this in dev tools console on a devoxx schedule page
+
+var jq = document.createElement('script');
+jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";
+document.getElementsByTagName('head')[0].appendChild(jq);
+
+jQuery.noConflict();
+
+var s = "";
+jQuery('.blackLink').each(function(){s = s + this.text + "\n"});
+s;
