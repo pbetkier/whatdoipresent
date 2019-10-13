e7002d858ce8b9ab2426d87d318442512b7cea41
Piotr Betkier <pbetkier@gmail.com>
2019-10-13 20:51:23 +0200

    Devoxx PL 2019 talks only

--- src/data/dataset.js
+++ src/data/dataset.js
@@ -1,7 +1,6 @@
-import devoxx18pl from './devoxx-pl-2018.txt';
-import devoxx18uk from './devoxx-uk-2018.txt';
+import devoxx19pl from './devoxx-pl-2019.txt';
 
-const lists = [devoxx18pl, devoxx18uk];
+const lists = [devoxx19pl];
 const allTalks = [].concat(...lists.map(x => x.split('\n')));
 
 export default new Set(allTalks);
